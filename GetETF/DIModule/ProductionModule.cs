2e6b2d556b087b5d872e9c64231accda61bf1159
jballard <jim.m.ballard@oracle.com>
2014-05-19 10:42:29 -0400

    First commit

---
+++ GetETF/DIModule/ProductionModule.cs
@@ -0,0 +1,20 @@
+﻿using Logger;
+using Core.Interface;
+using Ninject.Modules;
+//using Services.Interface;
+//using Services.Service;
+
+namespace GetETF.DIModule
+{
+    public class ProductionModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILogger>().To<ConsoleLogger>().InSingletonScope()
+                .WithConstructorArgument("loglevel", LogLevelEnum.Fatal);
+
+            //Bind<IMyFakeService>().To<MyFakeService>().InSingletonScope();
+            //Bind<IMyOtherService>().To<MyOtherService>().InSingletonScope();
+        }
+    }
+}
